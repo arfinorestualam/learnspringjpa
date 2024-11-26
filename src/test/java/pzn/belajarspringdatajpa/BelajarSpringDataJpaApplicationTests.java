9a498c03442e5c255ca10b71943fbafa8d867099
arfinorestualam <arfino.alam@gmail.com>
2024-11-26 16:59:38 +0700

    first

---
+++ src/test/java/pzn/belajarspringdatajpa/BelajarSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pzn.belajarspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
