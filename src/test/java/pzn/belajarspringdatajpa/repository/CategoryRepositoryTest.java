3a13c8606dae73cf272277e5c38729348f56ad39
arfinorestualam <arfino.alam@gmail.com>
2024-11-27 04:20:11 +0700

    testing crud from repository

---
+++ src/test/java/pzn/belajarspringdatajpa/repository/CategoryRepositoryTest.java
@@ -0,0 +1,52 @@
+package pzn.belajarspringdatajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pzn.belajarspringdatajpa.entity.Category;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testInsert() {
+        Category category = new Category();
+        category.setName("GADGET");
+
+        categoryRepository.save(category);
+
+        assertNotNull(category.getId());
+    }
+
+    @Test
+    void testUpdate() {
+        //check if the repository have data or not
+        Category category = categoryRepository.findById(4L).orElse(null);
+        assertNotNull(category);
+
+        //set new name
+        category.setName("GADGET MURAH");
+        //change the data
+        categoryRepository.save(category);
+
+        category = categoryRepository.findById(4L).orElse(null);
+        assertNotNull(category);
+        assertEquals("GADGET MURAH", category.getName());
+    }
+
+    @Test
+    void testDelete() {
+        Category category = categoryRepository.findById(4L).orElse(null);
+        assertNotNull(category);
+
+        categoryRepository.delete(category);
+        assertEquals(Optional.empty(),categoryRepository.findById(1L));
+    }
+}
